4be86bdae83fbe1e4d5c6c5a43d3035f6ef6c994
randiwest <randiwest@digitalshift.ca>
2015-12-01 08:03:47 -0800

    Changed querySelectorAll to getElementsByClassName

--- views/js/main.js
+++ views/js/main.js
@@ -436,7 +436,7 @@ var resizePizzas = function(size) {
             console.log("bug in sizeSwitcher");
     }
 
-    var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+    var randomPizzas = document.getElementsByClassName('randomPizzaContainer');
 
     for (var i = 0; i < randomPizzas.length; i++) {
         randomPizzas[i].style.width = newWidth + "%";
@@ -488,7 +488,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
 
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
